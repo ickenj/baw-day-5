e8691f4a5927322fb11e0eb3477307e5ac58095f
Joanne Icken <ickenj@gmail.com>
2012-10-26 20:47:03 -0400

    first commit

---
+++ Exercise1.php
@@ -0,0 +1,12 @@
+<?php
+$name="Susan";
+$birthdate="February 20, 1964";
+$favcolor="Blue";
+
+$br = "<br>";
+print "Homework for Saturday, October 6, 2012: Exercise 1" . $br . $br;
+print "My name is $name.$br";
+print "I was born $birthdate.$br";
+print "My favorite color is $favcolor.$br" . $br;
+print "I'm going to Utica College in January!";
+?>
